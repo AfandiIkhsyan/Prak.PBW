f30ff302aa725966b6d53a8bdeb7382516bf88d6
AfandiIkhsyan <afandiikhsyanalkarim@gmail.com>
2024-11-22 06:49:38 +0700

    add folder pert9/project part1

---
+++ pert9/project/routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'home');
+Route::view('/about', 'about');
+Route::view('/contact', 'contact');
+Route::view('/gallery', 'gallery');
